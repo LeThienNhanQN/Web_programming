6f90f0f52a79473cbe5ba68635097495f8e41434
LeThienNhanQN <tailieuhoctapvku@gmail.com>
2023-02-24 11:00:24 +0700

    project web

---
+++ QLBanHangOnline/application/config/routes.php
@@ -0,0 +1,5 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$route['default_controller'] = "dashboard";
+$route['backend$'] = "dashboard";
+$route['404_override'] = '';
